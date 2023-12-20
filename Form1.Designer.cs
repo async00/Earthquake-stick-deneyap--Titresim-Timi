d0ed25971735284fb1f8ec56d806c47626048b20
FlexPi <brberkayfodan@gmail.com>
2023-12-20 21:41:11 +0300

    mainupdate

---
+++ Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace EarthquakeStick_deneyap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.activestickdisplayint = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.servercheckdisplaystring = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(417, 33);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Deprem Çubuğu Kontrol Paneli\r\n";
+            // 
+            // activestickdisplayint
+            // 
+            this.activestickdisplayint.AutoSize = true;
+            this.activestickdisplayint.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.activestickdisplayint.Location = new System.Drawing.Point(12, 110);
+            this.activestickdisplayint.Name = "activestickdisplayint";
+            this.activestickdisplayint.Size = new System.Drawing.Size(244, 31);
+            this.activestickdisplayint.TabIndex = 3;
+            this.activestickdisplayint.Text = "Aktif Çubuk : 0.000\r\n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(12, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(231, 31);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Sunucu Durumu : \r\n";
+            // 
+            // servercheckdisplaystring
+            // 
+            this.servercheckdisplaystring.AutoSize = true;
+            this.servercheckdisplaystring.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.servercheckdisplaystring.ForeColor = System.Drawing.Color.Red;
+            this.servercheckdisplaystring.Location = new System.Drawing.Point(249, 198);
+            this.servercheckdisplaystring.Name = "servercheckdisplaystring";
+            this.servercheckdisplaystring.Size = new System.Drawing.Size(159, 31);
+            this.servercheckdisplaystring.TabIndex = 6;
+            this.servercheckdisplaystring.Text = "çalışmıyor :(";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(6, 300);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(1123, 251);
+            this.listBox1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 284);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Kontrol Paneli";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(465, -8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(664, 305);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(654, 231);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(341, 31);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Sesiniz enkazda kalmasın !";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(399, -1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "res";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.ForeColor = System.Drawing.Color.Red;
+            this.textBox1.Location = new System.Drawing.Point(6, 300);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(1123, 251);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1141, 563);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.servercheckdisplaystring);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.activestickdisplayint);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Deprem Çubuğu Kontrol Paneli (Sarsıntı Timi)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label activestickdisplayint;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label servercheckdisplaystring;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+
+namespace EarthquakeStick_deneyap
+{
+    public partial class Form1 : Form
+    {
+        private TcpListener server;
+        internal string ensongelenveri = string.Empty;
+
+        public Form1()
+        {
+           
+            InitializeComponent();
+
+        }
+    
+        void TCPthread()
+        {
+
+            try
+            {
+                string ipAddress = "127.0.0.1"; // localhost
+                int port = 80;
+                server = new TcpListener(IPAddress.Parse(ipAddress), port);
+                server.Start();
+                textBox1.Text=textBox1.Text+$"Sunucu Aktif Sesler Dinleniyor !!... (localhost:{port})";
+                activestickdisplayint.Text = "Aktif çubuk 0.001";
+                servercheckdisplaystring.ForeColor = System.Drawing.Color.Green;
+                servercheckdisplaystring.Text = "Çalışıyor :)";
+                while (true)
+                {
+                    TcpClient client = server.AcceptTcpClient();
+                    byte[] buffer = new byte[1024]; //maxcharsize
+                    int bytesRead;
+                    NetworkStream stream = client.GetStream();
+                    while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        
+                        string receivedData = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                        string clearedata = receivedData.Replace("X", "\r\n")+"--------------------------------------------------------------\r\n";
+                        textBox1.Text = textBox1.Text + clearedata;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"exception: {ex.Message}");
+                MessageBox.Show($"RAW exception: {ex.ToString()}");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //yeah open listener
+            textBox1.ReadOnly = true;
+
+            Thread maintcpthread = new Thread(TCPthread);
+            maintcpthread.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
